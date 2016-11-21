bc996a1835715423641abd01624c971d525a22ad
Jim Jones <itguy@koolaid.info>
2016-11-21 11:45:33 -0500

    Update Delete-WSUSClient.ps1

--- Delete-WSUSClient.ps1
+++ Delete-WSUSClient.ps1
@@ -5,6 +5,7 @@ $workstation = Read-Host -Prompt 'Input the computer name'
 # WSUS Connection Parameters:
 [String]$updateServer = "SERVERFQDN"
 [Boolean]$useSecureConnection = $False
+#Port Number varies. Look in WSUS GUI and it reports the correct port
 [Int32]$portNumber = 80
 
 # Load .NET assembly
