0a6b4d45ad4888613fb624016dc38e8a528431b9
Jim Jones <itguy@koolaid.info>
2021-03-05 13:49:15 -0500

    Update with header information, add cmdlet binding

--- Set-OSTags.ps1
+++ Set-OSTags.ps1
@@ -1,9 +1,23 @@
+<#
+.Synopsis
+  Assigns a general tag for Operating System to all VMs on a given vCenter based on the Guest.GuestFamily attribute of the VMs. Reason would be for credential selection for backup applications
+.Notes
+   Version: 0.5
+   Author: Jim Jones
+   Modified Date: 3/5/2021   
+.EXAMPLE
+  .\Set-OSTags.ps1 -vCenter = 'labvcsa1.lab.local'
+#>
+[CmdletBinding()]
+Param (
+    [string]$vCenter = "labvcsa1.lab.local"
+)
+
 Import-Module VMware.PowerCLI
-$vcenter = "labvcsa1.lab.local"
 $TagCategory = "OS"
-$WinTag = "Windows_Servers"
-$LinuxTag = "Linux_Servers"
-Connect-VIServer $vcenter
+$WinTag = "Windows"
+$LinuxTag = "Linux"
+Connect-VIServer $vCenter
 
 #Sanity Checks
 if(!(Get-TagCategory -Name $TagCategory)) {
